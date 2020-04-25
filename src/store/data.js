cd9c3c27b91004b99ebe634e9069bdafde531c57
Jacob Tan <jacobtyq@gmail.com>
2020-04-25 16:15:19 +0800

    Shift data into separate folder

---
+++ src/store/data.js
@@ -0,0 +1,45 @@
+const stores = [
+  {
+    name: "NTUC FairPrice",
+    url: "https://www.fairprice.com.sg/cart",
+    loading: false,
+    response: false,
+    serviceable: false,
+    dataCheck: false,
+    error: false,
+  },
+  {
+    name: "Sheng Shiong",
+    url: "https://www.allforyou.sg/cart",
+    loading: false,
+    response: false,
+    serviceable: false,
+    dataCheck: false,
+    error: false,
+  },
+  {
+    name: "Cold Storage",
+    url: "https://coldstorage.com.sg/checkout/cart",
+    loading: false,
+    response: false,
+    serviceable: false,
+    dataCheck: false,
+    error: false,
+  },
+  {
+    name: "Giant",
+    url: "https://giant.sg/checkout/cart",
+    loading: false,
+    response: false,
+    serviceable: false,
+    dataCheck: false,
+    error: false,
+  },
+  {
+    name: "Redmart",
+    site: "redmart.com",
+    url: "https://redmart-delivery-schedule.lazada.sg",
+  },
+];
+
+export default stores;
